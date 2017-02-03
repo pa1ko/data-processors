36f4a4ea353a660ce2b42bbdb2cf73251aad3b75
pkonarzewski <p.konarzewski@capitalservice.pl>
2017-02-03 16:45:34 +0100

    Add pesel utils

---
+++ processors/stdnum/pesel.py
@@ -0,0 +1,98 @@
+# -- coding: utf-8 -*-
+"""Extract information from Polish ID Number: PESEL."""
+
+import datetime
+import numpy as np
+import pandas as pd
+from dateutil.relativedelta import relativedelta
+
+from processors import dateutils
+
+
+def unify(items):
+    """Unifi format of pesel number."""
+    if items.dtype != 'O':
+        vitems = items.apply(str)
+
+    vitems = items.str.strip()
+    return vitems
+
+
+def is_valid(items):
+    """"Bool mask with True if pesel is valid."""
+    pesel_regex = r'^(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)(\d)$'
+
+    digit_df = (items.str.extract(pesel_regex, expand=True)
+                .applymap(float)  # float because of posible NaN's
+               )
+
+    controlsum = (9 * digit_df.iloc[:, 0].values +
+                  7 * digit_df.iloc[:, 1].values +
+                  3 * digit_df.iloc[:, 2].values +
+                  1 * digit_df.iloc[:, 3].values +
+                  9 * digit_df.iloc[:, 4].values +
+                  7 * digit_df.iloc[:, 5].values +
+                  3 * digit_df.iloc[:, 6].values +
+                  1 * digit_df.iloc[:, 7].values +
+                  9 * digit_df.iloc[:, 8].values +
+                  7 * digit_df.iloc[:, 9].values
+                 ) % 10
+
+    return digit_df.iloc[:, 10] == controlsum
+
+
+def gender(items, gender_sym=None):
+    """Extract gender from pesel.
+
+    Decode 10th number of pesel to return gender:
+    on even women, odd on men.
+    gender_sym: tuple ('Female symbol', 'Male symbol'), if None will return
+     1 = Female, 2 = Male.
+    """
+    if gender_sym is None:
+        gender_sym = (1, 2)
+
+    gender_digit = dict(
+        zip([str(x) for x in range(10)],
+            [gender_sym[0] if x % 2 == 0 else gender_sym[1] for x in range(10)])
+        )
+
+    gender_s = (items.where(is_valid(items))
+                .str.slice(start=9, stop=10)
+                .map(gender_digit))
+    return gender_s
+
+
+def birth_date(items):
+    """Extract birth date from pesel."""
+    mth_century_substract = {
+        '18': 80,
+        '19': 0,
+        '20': 20,
+        '21': 40,
+        '22': 60
+    }
+
+    vitems = (items.where(is_valid(items))
+              .str.extract(pat=r'^(\d\d)(\d\d)(\d\d)', expand=True)
+              .rename(columns={0: 'year', 1: 'month', 2: 'day'})
+              .assign(month=lambda x: x['month'].apply(float))
+              .assign(century=lambda x: pd.cut(x['month'],
+                                               bins=[0, 12, 32, 52, 72, 92],
+                                               labels=['19', '20', '21', '22', '18']))
+              .assign(year=lambda x: x['century'].str.cat(x['year']))
+              .assign(month=lambda x: x['month'] - x['century'].map(mth_century_substract))
+             )
+
+    return pd.to_datetime(vitems[['year', 'month', 'day']])
+
+
+def age(items, ex_date):
+    """Get person age based on the pesel and check date.
+
+    Simillary to other date related funcs you can pass str, datetime
+    or series. Function will try to broadcast scalars.
+    """
+    return dateutils.datediff(date1=dateutils.unify(ex_date),
+                              date2=birth_date(items),
+                              interval='Y')
---
+++ tests/test_pesel.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+import unittest
+import pandas as pd
+from numpy import nan as NaN
+import pandas.util.testing as pdtest
+from datetime import datetime
+
+from processors.stdnum import pesel
+
+
+class PeselTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.pesel_df = pd.DataFrame(
+            [('44112607890', 2, datetime(1944, 11, 26), True),
+             ('70102108696', 2, datetime(1970, 10, 21), True),
+             ('01320107989', 1, datetime(2001, 12, 1), True),
+             ('99851201182', 1, datetime(1899, 5, 12), True),
+             ('3301051236', NaN, pd.NaT, False),
+             ('70102108691', NaN, pd.NaT, False),
+             ('10102108696', NaN, pd.NaT, False),
+             ('123', NaN, pd.NaT, False)],
+            columns=['pesel', 'gender', 'birth_date', 'valid']
+        )
+
+    def test_pesel_validation(self):
+        tested = pesel.is_valid(self.pesel_df['pesel'])
+        pdtest.assert_series_equal(tested, self.pesel_df['valid'],
+                                   check_names=False)
+
+    def test_unify(self):
+        tdf = pd.DataFrame([(' 123  ', '123'),
+                            ('123', '123'),
+                            ('    123', '123')],
+                           columns=['dirty', 'clean'])
+
+        tested = pesel.unify(tdf['dirty'])
+        pdtest.assert_series_equal(tested, tdf['clean'],
+                                   check_names=False)
+
+    def test_pesel_gender(self):
+        tested = pesel.gender(self.pesel_df['pesel'])
+        pdtest.assert_series_equal(tested, self.pesel_df['gender'],
+                                   check_names=False)
+
+    def test_pesel_map_gender(self):
+        tested = pesel.gender(pd.Series(['44112607890', '01320107989', '123']),
+                              gender_sym=('F', 'M'))
+        pdtest.assert_series_equal(tested, pd.Series(['M', 'F', NaN]),
+                                   check_names=False)
+
+    def test_pesel_birth_date(self):
+        tested = pesel.birth_date(self.pesel_df['pesel'])
+        pdtest.assert_series_equal(tested, self.pesel_df['birth_date'],
+                                   check_names=False)
+
+    def test_pesel_age(self):
+        tested = pesel.age(self.pesel_df['pesel'], '2016-12-31')
+        pdtest.assert_series_equal(tested, pd.Series([72., 46., 15., 117., NaN, NaN, NaN, NaN]),
+                                   check_names=False)
