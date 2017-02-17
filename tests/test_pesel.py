6b7416bf9b675c5a916172c61b8afb5d156b7937
pkonarzewski <p.konarzewski@capitalservice.pl>
2017-02-17 14:54:29 +0100

    fix pesel tests

--- tests/test_pesel.py
+++ tests/test_pesel.py
@@ -12,15 +12,21 @@ class PeselTestCase(unittest.TestCase):
 
     def setUp(self):
         self.pesel_df = pd.DataFrame(
-            [('44112607890', 2, datetime(1944, 11, 26), True),
-             ('70102108696', 2, datetime(1970, 10, 21), True),
-             ('01320107989', 1, datetime(2001, 12, 1), True),
-             ('99851201182', 1, datetime(1899, 5, 12), True),
-             ('3301051236', NaN, pd.NaT, False),
-             ('70102108691', NaN, pd.NaT, False),
-             ('10102108696', NaN, pd.NaT, False),
-             ('123', NaN, pd.NaT, False)],
-            columns=['pesel', 'gender', 'birth_date', 'valid']
+            # valid
+            [('44112607890', 2, datetime(1944, 11, 26), True, 72.),
+             ('70102108696', 2, datetime(1970, 10, 21), True, 46.),
+             ('01320107989', 1, datetime(2001, 12, 1), True, 15.),
+             ('99851201182', 1, datetime(1899, 5, 12), True, 117.),
+             # invalid
+             # too long
+             ('998512011821', NaN, pd.NaT, False, NaN),
+             # to short
+             ('3301051236', NaN, pd.NaT, False, NaN),
+             # wrong checksum
+             ('70102108691', NaN, pd.NaT, False, NaN),
+             ('10102108696', NaN, pd.NaT, False, NaN),
+             ('123', NaN, pd.NaT, False, NaN)],
+            columns=['pesel', 'gender', 'birth_date', 'valid', 'age20161231']
         )
 
     def test_pesel_validation(self):
@@ -56,7 +62,7 @@ class PeselTestCase(unittest.TestCase):
 
     def test_pesel_age(self):
         tested = pesel.age(self.pesel_df['pesel'], '2016-12-31')
-        pdtest.assert_series_equal(tested, pd.Series([72., 46., 15., 117., NaN, NaN, NaN, NaN]),
+        pdtest.assert_series_equal(tested, self.pesel_df['age20161231'],
                                    check_names=False)
 
     def test_numeric_pesel(self):
